843ccd30cc58455b63c2b26a4da2cabe6020e609
Kazumasa Kohtaka <kkohtaka@gmail.com>
2021-11-15 15:25:53 +0900

    Allow Etcd controller to access Secret resources

--- controllers/etcd_controller.go
+++ controllers/etcd_controller.go
@@ -49,6 +49,7 @@ type EtcdReconciler struct {
 //+kubebuilder:rbac:groups=kubernetesimal.kkohtaka.org,resources=etcds/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=kubernetesimal.kkohtaka.org,resources=etcds/finalizers,verbs=update
 //+kubebuilder:rbac:groups=kubevirt.io,resources=virtualmachineinstances,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
 
 const (
 	finalizerName = "kubernetesimal.kkohtaka.org/finalizer"
