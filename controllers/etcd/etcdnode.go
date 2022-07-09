eeef94a32e72c5054c5fe1b3d23d9e41f6e142be
Kazumasa Kohtaka <kkohtaka@gmail.com>
2022-07-09 17:54:55 +0900

    etcd: Remove orphaned EtcdNodes

--- controllers/etcd/etcdnode.go
+++ controllers/etcd/etcdnode.go
@@ -2,14 +2,65 @@ package etcd
 
 import (
 	"context"
+	"fmt"
 
 	kubernetesimalv1alpha1 "github.com/kkohtaka/kubernetesimal/api/v1alpha1"
 	"github.com/kkohtaka/kubernetesimal/controller/finalizer"
 	"github.com/kkohtaka/kubernetesimal/observability/tracing"
 	"go.opentelemetry.io/otel/trace"
+	"k8s.io/apimachinery/pkg/labels"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
+
+func removeOrphanNodes(
+	ctx context.Context,
+	c client.Client,
+	obj client.Object,
+	status *kubernetesimalv1alpha1.EtcdStatus,
+) error {
+	var span trace.Span
+	ctx, span = tracing.FromContext(ctx).Start(ctx, "removeOrphanNodes")
+	defer span.End()
+
+	logger := log.FromContext(ctx)
+
+	childNodeNames := map[string]struct{}{}
+	for _, nodeRef := range status.NodeRefs {
+		childNodeNames[nodeRef.Name] = struct{}{}
+	}
+
+	var nodes kubernetesimalv1alpha1.EtcdNodeList
+	if err := c.List(ctx, &nodes, &client.ListOptions{
+		Namespace:     obj.GetNamespace(),
+		LabelSelector: labels.Everything(),
+	}); err != nil {
+		return fmt.Errorf("unable to list EtcdNodes: %w", err)
+	}
+
+	for i := range nodes.Items {
+		node := &nodes.Items[i]
+		if _, ok := childNodeNames[node.GetName()]; ok {
+			continue
+		}
+		for _, ref := range node.OwnerReferences {
+			if ref.Controller == nil || !*ref.Controller {
+				continue
+			}
+			if ref.UID != obj.GetUID() {
+				continue
+			}
+
+			// Here, this EtcdNode is owned by the Etcd but not listed in Status.NodeRefs.
+			logger.Info("Orphaned EtcdNode was found.", "etcdnode", node.GetName())
+			if err := finalizeEtcdNode(ctx, c, obj.GetNamespace(), node.GetName()); err != nil {
+				return fmt.Errorf("unable to finalize a EtcdNode: %w", err)
+			}
+		}
+	}
+
+	return nil
+}
 
 func finalizeEtcdNodes(
 	ctx context.Context,
